79743b39d8cd440746871559136f3056b6a5cab0
PleaseIJustWantAUsername <116348294+PleaseIJustWantAUsername@users.noreply.github.com>
2022-10-21 19:59:53 +1030

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box;
+  
+
+function setup() {
+  createCanvas(600,400);
+  box = createSprite(250, 200, 100,50);
+  box.shapeColor = "lightblue"
+}
+
+function draw() 
+{
+  background("blue");
+  drawSprites();
+}
+
+
+
+
